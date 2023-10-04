38c34dff98eb0e0c801ea066e7d19d7a79b46e2f
Camila <camila.rodriguez.aguila@gmail.com>
2023-10-03 21:44:50 -0700

    Cambios
    Nombre de Main

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 public class Main {
-    public static void main (String args[]){
-        Run.Correr();
+    public static void main (String [] args){
+        Run in = new Run();
+        in.Correr();
+
     }
 }
